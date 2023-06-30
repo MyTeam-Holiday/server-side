7781da15e6c0c592dfc3e6cfd917612f31798f00
Artur <arturcentner@gmail.com>
2023-06-30 19:16:48 +0200

    Add authcontroller

---
+++ myteam.holiday.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myteam.holiday.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet, Route("GoogleOAuth")]
+        public IActionResult GoogleOAuth()
+        {
+            return Challenge(new AuthenticationProperties
+            { RedirectUri = "https://localhost:5001/swagger/index.html" },
+                authenticationSchemes: new string[] { "google" });
+        }
+    }
+}

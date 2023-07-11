3777047d31891924863056f3ca32c41717eca35a
ValeraBro <138037567+ValeraBro@users.noreply.github.com>
2023-07-12 00:42:06 +0300

    Update Program.cs
    Я комичу рядом навсякий, в общем, что тут происходит - видно в коде, расписал бы, но капец поздно по времени. Вот такой вопрос, в проекте 1 главный джейсон и три вылезают из под него. Если зашифровать главный, то как я понимаю, три остальных вряд ли будут шифрованы, поэтому можно сделать массив и прогонять файлы по отдельности, сделаю это завтра. Фигачу напрямую, ибо не все скачал для проекта похоже и не дает нормально открыть. Пофикшу такое.

--- myteam.holiday.WebApi/Program.cs
+++ myteam.holiday.WebApi/Program.cs
@@ -15,6 +15,21 @@ namespace myteam.holiday.init
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // Получаем ключ шифрования
+        key = GetEncryptionKey();
+
+      string appSettingsFileName = "appsettings.json";
+    string appSettingsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, appSettingsFileName);
+
+
+        // Зашифровываем appsettings.json, если он не зашифрован
+        if (!IsEncrypted(appSettingsFilePath))
+        {
+            EncryptAppSettings(appSettingsFilePath);
+        }
+
+        // Расшифровываем appsettings.json и выполняем остальную логику приложения
+        DecryptAppSettings(appSettingsFilePath);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,5 +38,95 @@ namespace myteam.holiday.init
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+private static byte[] GetEncryptionKey()
+    {
+        string keyString = Environment.GetEnvironmentVariable(KeyEnvironmentVariableName);
+        if (string.IsNullOrEmpty(keyString))
+        {
+            // Если переменная среды не установлена, генерируем новый ключ
+            byte[] generatedKey = GenerateKey();
+            Environment.SetEnvironmentVariable(KeyEnvironmentVariableName, Convert.ToBase64String(generatedKey));
+            return generatedKey;
+        }
+        else
+        {
+            // Если переменная среды установлена, используем ее значение
+            return Convert.FromBase64String(keyString);
+        }
+    }
+
+    private static byte[] GenerateKey()
+    {
+        using (AesManaged aes = new AesManaged())
+        {
+            aes.GenerateKey();
+            return aes.Key;
+        }
+    }
+
+    private static bool IsEncrypted(string filePath)
+    {
+        string fileContent = File.ReadAllText(filePath);
+        return !string.IsNullOrWhiteSpace(fileContent) && fileContent.TrimStart().StartsWith("encrypted:");
+    }
+
+    private static void EncryptAppSettings(string filePath)
+    {
+        string plainText = File.ReadAllText(filePath);
+
+        byte[] encryptedBytes;
+        using (AesManaged aes = new AesManaged())
+        {
+            aes.Key = key;
+            aes.IV = aes.Key;
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(plainTextBytes, 0, plainTextBytes.Length);
+                }
+                encryptedBytes = ms.ToArray();
+            }
+        }
+
+        string encryptedText = Convert.ToBase64String(encryptedBytes);
+        File.WriteAllText(filePath, "encrypted:" + encryptedText);
+    }
+
+    private static void DecryptAppSettings(string filePath)
+    {
+        string fileContent = File.ReadAllText(filePath);
+        if (fileContent.StartsWith("encrypted:"))
+        {
+            string encryptedText = fileContent.Substring("encrypted:".Length);
+
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+            string decryptedText;
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Key = key;
+                aes.IV = aes.Key;
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedBytes, 0, encryptedBytes.Length);
+                    }
+                    byte[] decryptedBytes = ms.ToArray();
+                    decryptedText = Encoding.UTF8.GetString(decryptedBytes);
+                }
+            }
+
+            File.WriteAllText(filePath, decryptedText);
+        }
+
+
+        
     }
 }

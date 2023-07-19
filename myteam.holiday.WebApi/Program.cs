fd74d610744c332d9aad50bdd22d1d1f17caac5d
ValeraBro <138037567+ValeraBro@users.noreply.github.com>
2023-07-19 23:25:09 +0300

    Update Program.cs
    Не понял про дешифровку при билде или типо того, тормознут немного, поэтому тут онли шифровка, но сама возможность дешифровки есть в новом классе.

--- myteam.holiday.WebApi/Program.cs
+++ myteam.holiday.WebApi/Program.cs
@@ -1,6 +1,5 @@
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -15,13 +14,31 @@ namespace myteam.holiday.init
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
 
+            string[] jsonFilePaths = new string[]
+            {
+                "appsettings.dev.json",
+                "пappsettings.prod.json",
+                "appsettings.uat.json",
+            
+            };
+            
+            foreach (string jsonFilePath in jsonFilePaths)
+            {
+                if (!IsEncrypted(jsonFilePath))
+                {
+                    Crypt.EncryptFile(jsonFilePath);
+                }
+            }
+            
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
     }
+    
 }

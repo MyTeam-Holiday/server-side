afebf01daab701332a2b27f3509c7035feba8a80
Dimitrij <kvarkas@gmail.com>
2023-07-19 17:40:57 +0100

    #20 add

---
+++ myteam.tools/setenv.ps1
@@ -0,0 +1,14 @@
+# Setting an environment variables
+# Syntax: $env:<variable_name> = "<value>"
+
+$env:mySecureVariable = ''
+$env:server_ip = ''
+$env:db_user_id = ''
+$env:db_password = ''
+$env:db_name = ''
+$env:client_id = ''
+$env:client_secret = ''
+$env:client_domain = ''
+$env:email_host = ''
+$env:email_address = ''
+$env:email_password = ''

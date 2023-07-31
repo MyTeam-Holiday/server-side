4b9a90edabedc7d62525bc98f856e300a3c3a6fe
ValeraBro <138037567+ValeraBro@users.noreply.github.com>
2023-08-01 00:14:48 +0300

    Add files via upload
    Это пункт - прога, которая при билде шифрует джейсон. НО я не врубил какое название будет после билда, поэтому посмотри код, исходники фул тут. Версия языка 8.0,  дот нет 4.8.

---
+++ myteam.holiday.WebApi/DataCrypt&Decryptor/DataCrypt&Decryptor/DataEncryptor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DataCrypt_Decryptor
+{
+    internal class DataEncryptor
+    {
+        private string key;
+        private string iv;
+
+        public DataEncryptor(string key, string iv)
+        {
+            this.key = key;
+            this.iv = iv;
+        }
+
+        // Метод для шифрования данных
+        public string Encrypt(string data)
+        {
+            using Aes aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using MemoryStream msEncrypt = new MemoryStream();
+            using CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using StreamWriter swEncrypt = new StreamWriter(csEncrypt);
+
+            swEncrypt.Write(data);
+
+            return Convert.ToBase64String(msEncrypt.ToArray());
+        }
+
+        // Метод для дешифрования данных
+        public string Decrypt(string encryptedData)
+        {
+            byte[] cipherText = Convert.FromBase64String(encryptedData);
+            using Aes aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using MemoryStream msDecrypt = new MemoryStream(cipherText);
+            using CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using StreamReader srDecrypt = new StreamReader(csDecrypt);
+
+            return srDecrypt.ReadToEnd();
+        }
+        public static byte[] GenerateKey()
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.GenerateKey();
+                return aes.Key;
+            }
+        }
+
+        public static byte[] GenerateIV()
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.GenerateIV();
+                return aes.IV;
+            }
+        }
+    }
+}
---
+++ myteam.holiday.WebApi/DataCrypt&Decryptor/DataCrypt&Decryptor/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCrypt_Decryptor
+{
+   
+    internal class Program
+    {
+        public static string key;
+        public static string iv;
+        static void Main(string[] args)
+        {
+            DataEncryptor.GenerateKey();
+            DataEncryptor.GenerateIV();
+            string jsonData = File.ReadAllText("appsettings.json");
+            key = "your_secret_key";
+             iv = "your_initialization_vector";
+
+            DataEncryptor dataEncryptor = new DataEncryptor(key, iv);
+
+            // Путь к файлу appsettings.json , реальное название хз какое будет, поправьте, кто увидет, там же файл после билда будет или типо того, ну вы поняли
+            string appSettingsPath = "appsettings.json";
+
+            if (File.Exists(appSettingsPath))
+            {
+                // Чтение данных из файла
+                string jsonDataFromFile = File.ReadAllText(appSettingsPath);
+
+                // Проверка наличия зашифрованных данных
+                bool isEncrypted = jsonDataFromFile.Contains("Encrypted:");
+
+                if (!isEncrypted)
+                {
+                    // Шифрование данных
+                    string encryptedData = dataEncryptor.Encrypt(jsonData);
+
+                    // Добавление пометки о зашифрованных данных
+                    jsonDataFromFile = $"Encrypted: {encryptedData}";
+
+                    // Сохранение обратно в файл
+                    File.WriteAllText(appSettingsPath, jsonDataFromFile);
+                    Console.WriteLine("Данные зашифрованы и сохранены в appsettings.json.");
+                }
+                else
+                {
+                    // Разбираем пометку для получения зашифрованных данных
+                    int startIndex = jsonDataFromFile.IndexOf(": ") + 2;
+                    string encryptedData = jsonDataFromFile.Substring(startIndex);
+
+                    // Дешифрование данных
+                    string decryptedData = dataEncryptor.Decrypt(encryptedData);
+                    jsonDataFromFile = $"Decrypted: {encryptedData}";
+                    File.WriteAllText(appSettingsPath, jsonDataFromFile);
+                    Console.WriteLine("Данные расшифрованы и сохранены в appsettings.json.");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл appsettings.json не найден.");
+            }
+        }
+    }
+}
